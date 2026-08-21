a69a94f4f2efb99379342831b06e85eb55eb9a95
ritajiay <33677965+ritajiay@users.noreply.github.com>
2026-08-21 16:08:07 +0800

    first commit

---
+++ astro.config.mjs
@@ -0,0 +1,9 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+  output: 'static', // 確保是 static 以適應 GitHub Pages
+  site: 'https://ritajiay.github.io',
+  base: '/collection/',
+});
